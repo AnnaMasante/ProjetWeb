a32e86a8be971db6b0796d85476ef6c7295e128b
AnnaMasante <spatchoun@gmail.com>
2020-02-25 15:25:35 +0100

    Add user

--- server.js
+++ server.js
@@ -25,6 +25,56 @@ app.get('/', function(request, response) {
     response.sendFile(path.join(__dirname + '/views/login.html'));
 });
 
+//create user Model
+const Personne = sequelize.define(
+    'Personne',{
+        idPers:{
+            type: Sequelize.INTEGER,
+            primaryKey:true,
+            autoIncrement:true
+        },
+        prenom:{
+            type:Sequelize.STRING
+        },
+        nom:{
+            type:Sequelize.STRING
+        },
+        numTel:{
+            type:Sequelize.INTEGER
+        },
+        enRecherche:{
+            type:Sequelize.TINYINT,
+            defaultValue:0
+        },
+        mail:{
+            type:Sequelize.STRING
+        },
+        mdp:{
+            type:Sequelize.STRING
+        },
+        sexe:{
+            type:Sequelize.STRING
+        }
+    });
+
+Personne.sync()
+    .then(()=>console.log('Table de Personne créée'))
+    .catch(err => console.log('Oups ça ne marche pas'));
+
+//Fonctions d'aide
+const createUSer = async({idPers,prenom,nom,numTel, enRecherche,mail,mdp,sexe})=>{
+    return await Personne.create({idPers,prenom,nom,numTel, enRecherche,mail,mdp,sexe});
+};
+
+const getAllUsers=async()=>{
+    return await Personne.findAll();
+};
+
+const getUser = async obj => {
+    return await Personne.findOne({
+        where:obj,
+    });
+};
     //On démarre le serveur
 app.listen(3000, function(){
     console.log('Express running sur le port 3000');
